b7ba75365116cc3e0b1a18b0d5f274f602961af3
tobylai-toby <toby_lai@126.com>
2025-08-09 12:18:11 +0800

    fix tray

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,6 @@
-import test from 'ava'
-
-import { plus100 } from '../index'
-
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+import test from 'ava';
+import {SystemTray} from "../index";
+test('ok', (t) => {
+  const tray = new SystemTray();
+  t.truthy(tray);
 })
